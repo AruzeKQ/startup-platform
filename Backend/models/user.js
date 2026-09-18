7874e0f0c09ffeb06d14a21ce0ebf249f070f2cf
AruzeKQ <khaiquang0103@gmail.com>
2026-09-19 00:31:10 +0700

    fixed validate for T.A fat

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -49,7 +49,7 @@ function userRegisterValidate(userInfo) {
         name: Joi.string().min(5).max(1024).required(),
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(1024).required(),
-
+        role: Joi.string().required(),
     });
     return Schema.validate(userInfo);
 }
--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -4,11 +4,10 @@ const taskController = require('../controllers/taskController');
 const auth = require('../middlewares/auth');
 
 router.post('/', auth, taskController.createTask);
-router.get('/my-tasks', auth, taskController.getTasksByUser);
+router.get('/my-tasks', auth, taskController.getTaskById);
 router.get('/:taskId', auth, taskController.getTaskById);
 router.put('/:taskId', auth, taskController.updateTask);
 router.delete('/:taskId', auth, taskController.deleteTask);
 
 module.exports = router;
 
-module.exports = router;
